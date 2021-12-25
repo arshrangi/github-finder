09957ff7665023920eeb867dcd1a502f540f85fb
Arshdeep Rangi <arshdeeprangi@tactica.ca>
2021-12-25 11:17:14 -0600

    Update ReadMe and add screenshots

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -8,7 +8,7 @@ class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
     this.props.getUserRepos(this.props.match.params.login);
-  }
+  } 
   render() {
     const {
       name,
